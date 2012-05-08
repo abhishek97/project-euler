c834de589eed7bcdb814e0e97c8250158fa2f124
Charles Leifer <coleifer@gmail.com>
2012-05-08 14:49:18 -0500

    A little more verbose

--- problem85.py
+++ problem85.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import math
 
 def ct(m, n):
     return ((n*n + n) * (m*m + m)) / 4
@@ -11,9 +12,14 @@ bits = None
 
 # know that 1, 2000 = 2001000, so we can assume
 # upper bound is 2000
+# alex suggested not doing the quadratic inline was cheating so
+# here it is, assuming m = 1, we end up with
+# (n^2+n)(1) - 4000000 = 0
+quad = lambda a, b, c: (-b + math.sqrt(b**2 - 4*a*c))/(2*a)
+upper = int(quad(1, 1, -4000000))
 
-for i in range(2, 2000):
-    for j in range(i, 2000):
+for i in range(1, upper):
+    for j in range(i, upper):
         x = ct(i, j)
         new_diff = abs(2000000 - x)
         if new_diff < diff:
